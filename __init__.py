e7386edf865db9315417f5d756946ba59dad98e1
drdr xp <drdr.xp@gmail.com>
2020-05-22 16:16:17 +0800

    bump version 0.2.2

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ from .proc import command_ex
 from .proc import shell_script
 from .proc import start_process
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     'CalledProcessError',
