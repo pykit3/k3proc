3bc013f7800c8806b55e011ca62e08457f675ffa
drdr xp <drdr.xp@gmail.com>
2020-11-25 01:36:10 +0800

    release: v0.2.8

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 import setuptools
 setuptools.setup(
     name="k3proc",
-    packages=["."],
-    version="0.2.7",
+    packages=["k3proc"],
+    version="0.2.8",
     license='MIT',
     description='easy to use `Popen`',
     long_description="# k3proc\n\n[![Build Status](https://travis-ci.com/pykit3/k3proc.svg?branch=master)](https://travis-ci.com/pykit3/k3proc)\n[![Documentation Status](https://readthedocs.org/projects/k3proc/badge/?version=stable)](https://k3proc.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3proc)](https://pypi.org/project/k3proc)\n\neasy to use `Popen`\n\nk3proc is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3proc\n```\n\n# Synopsis\n\n```python\n>>> returncode, out, err = k3proc.shell_script('ls / | grep bin')\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3",
