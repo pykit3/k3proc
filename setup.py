4f50ec2265c323f27c689e138c412609662391a1
drdr xp <drdr.xp@gmail.com>
2020-12-18 13:40:39 +0800

    release: v0.2.10

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(
     name="k3proc",
     packages=["k3proc"],
-    version="0.2.9",
+    version="0.2.10",
     license='MIT',
     description='easy to use `Popen`',
-    long_description="# k3proc\n\n[![Build Status](https://travis-ci.com/pykit3/k3proc.svg?branch=master)](https://travis-ci.com/pykit3/k3proc)\n[![Documentation Status](https://readthedocs.org/projects/k3proc/badge/?version=stable)](https://k3proc.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3proc)](https://pypi.org/project/k3proc)\n\neasy to use `Popen`\n\nk3proc is a component of [pykit3] project: a python3 toolkit set.\n\n\n# Install\n\n```\npip install k3proc\n```\n\n# Synopsis\n\n```python\n>>> returncode, out, err = k3proc.shell_script('ls / | grep bin')\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3",
+    long_description="# k3proc\n\n[![Build Status](https://travis-ci.com/pykit3/k3proc.svg?branch=master)](https://travis-ci.com/pykit3/k3proc)\n![Python package](https://github.com/pykit3/k3proc/workflows/Python%20package/badge.svg)\n[![Documentation Status](https://readthedocs.org/projects/k3proc/badge/?version=stable)](https://k3proc.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3proc)](https://pypi.org/project/k3proc)\n\neasy to use `Popen`\n\nk3proc is a component of [pykit3] project: a python3 toolkit set.\n\n\nk3proc is utility to create sub process.\n\nExecute a shell script::\n\n    >>> returncode, out, err = k3proc.shell_script('ls / | grep bin')\n\nRun a command::\n\n    # Unlike the above snippet, following statement does not start an sh process.\n    returncode, out, err = k3proc.command('ls', 'a*', cwd='/usr/local')\n\n\n\n\n# Install\n\n```\npip install k3proc\n```\n\n# Synopsis\n\n```python\n>>> returncode, out, err = k3proc.shell_script('ls / | grep bin')\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3",
     long_description_content_type="text/markdown",
     author='Zhang Yanpo',
     author_email='drdr.xp@gmail.com',
